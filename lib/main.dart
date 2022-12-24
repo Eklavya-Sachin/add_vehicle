ac36b4f9a3adc7d23eb06c36fb249ef4458ee79d
Sachin Kumar <ojaz07860@gmail.com>
2022-12-24 23:09:47 +0530

    Initialized Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:path_provider/path_provider.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
+import 'app/data/vehicle_model.dart';
+import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final document = await getApplicationDocumentsDirectory();
+  await Hive.initFlutter(document.path);
+  Hive.registerAdapter(VehicleModelAdapter());
+  await Hive.openBox("vehicle");
+
   runApp(
     GetMaterialApp(
       title: "Vehicle Info",
